482a3d099fae17130f940119a87109fda047d9d7
maxefimov <efimov.academy.top@gmail.com>
2023-02-22 21:18:53 +0300

    Добавьте файлы проекта.

---
+++ NetDelegatsUsingApp/Account.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetDelegatsUsingApp
+{
+    
+    internal class AccountEventArgs
+    {
+        public string Message { get; set; }
+        public int Amount { get; set; }
+        public int TotalAmount { get; set; }
+
+        public AccountEventArgs(string message, int amount, int totalAmount)
+        {
+            Message = message;
+            Amount = amount;
+            TotalAmount = totalAmount;
+        }
+    }
+
+    internal class Account
+    {
+        int amount;
+
+        public string Name { set; get; }
+
+        public delegate void AccountHandler(Account sender, AccountEventArgs args);
+
+        public event AccountHandler? Notify;
+
+        /*
+        public event AccountHandler? Notify
+        {
+            add
+            {
+                notify += value;
+                Console.WriteLine($"add method: {value?.Method.Name}");
+            }
+            remove
+            {
+                notify -= value;
+                Console.WriteLine($"remove method: {value?.Method.Name}");
+            }
+        }
+        */        
+
+        //public AccountHandler Handler { set; private get; }
+
+        //AccountHandler? handler;
+
+        //public void AddHandler(AccountHandler handler)
+        //{
+        //    this.handler += handler;
+        //}
+
+        //public void RemoveHandler(AccountHandler handler)
+        //{
+        //    this.handler -= handler;
+        //}
+
+        public Account(int amount)
+        {
+            this.amount = amount;
+        }
+        public void Put(int amount)
+        {
+            this.amount += amount;
+            //handler?.Invoke($"At account add amount {amount}, total accaunt amount = {this.amount}");
+            //notify?.Invoke($"At account add amount {amount}, total accaunt amount = {this.amount}");
+            Notify?.Invoke(this, new("Amount put", amount, this.amount));
+        }
+        public void Take(int amount)
+        {
+            if (this.amount >= amount)
+            {
+                this.amount -= amount;
+                //notify?.Invoke($"At account takes amount {amount}, total accaunt amount = {this.amount}");
+                //handler?.Invoke($"At account takes amount {amount}, total accaunt amount = {this.amount}");
+                Notify?.Invoke(this, new("Amount Take", amount, this.amount));
+            }
+            else
+                //notify?.Invoke($"Total amount {this.amount}");
+                Notify?.Invoke(this, new("Error take", amount, this.amount));
+
+        }
+    }
+}
---
+++ NetDelegatsUsingApp/Program.cs
@@ -0,0 +1,120 @@
+﻿using System.Reflection;
+
+namespace NetDelegatsUsingApp
+{
+    public delegate void Message(string message);
+
+    public delegate T Operation<T>(T a, T b);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AccountExample();
+
+        }
+
+        static T Calc<T>(T a, T b, Operation<T> op)
+        {
+            return op(a, b);
+        }
+        static void AccountExample()
+        {
+            Account account1 = new(1000) { Name = "Bob" };
+            account1.Notify += ConsoleMessage;
+            account1.Notify += ConsoleColorMessage;
+
+            account1.Put(200);
+            account1.Take(500);
+            account1.Take(600);
+
+            account1.Notify -= ConsoleMessage;
+
+            account1.Take(300);
+
+            Console.WriteLine();
+
+            Account account2 = new(1000) { Name = "Joe" };
+            account2.Notify += ConsoleMessage;
+            account2.Notify += ConsoleColorMessage;
+
+            account2.Put(200);
+            account2.Take(500);
+            account2.Take(600);
+
+            account2.Notify -= ConsoleMessage;
+
+            account2.Take(300);
+
+
+            void ConsoleMessage(Account sender, AccountEventArgs e)
+            {
+                Console.WriteLine($"Info for account: {sender.Name}");
+                Console.WriteLine(e.Message);
+                Console.WriteLine($"Amount put: {e.Amount}");
+                Console.WriteLine($"Total account amount: {e.TotalAmount}");
+            }
+
+            void ConsoleColorMessage(Account sender, AccountEventArgs e)
+            {
+                var oldColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine($"Info for account: {sender.Name}");
+                Console.WriteLine(e.Message);
+                Console.WriteLine($"Amount put: {e.Amount}");
+                Console.WriteLine($"Total account amount: {e.TotalAmount}");
+
+                Console.ForegroundColor = oldColor;
+            }
+        }
+
+        static void AnonimMetodsExample()
+        {
+            Message? message1 = delegate (string message)
+            {
+                Console.WriteLine(message);
+            };
+
+            Message? message2 = (message) =>
+            {
+                var oldColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(message);
+                Console.ForegroundColor = oldColor;
+            };
+
+            message1?.Invoke("Hello world");
+
+            message2 -= (message) =>
+            {
+                var oldColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(message);
+                Console.ForegroundColor = oldColor;
+            };
+
+
+            Message? messages = new(message1);
+            messages += message2;
+
+            messages("AAAAAAAA");
+
+            messages -= message1;
+
+            messages("BBBBBBBB");
+
+
+            message2?.Invoke("Good by world");
+
+            int result = Calc(10, 20, delegate (int a, int b)
+            {
+                return a + b;
+            });
+
+
+
+            result = Calc(10, 20, (a, b) => a * b);
+        }
+    }
+}
